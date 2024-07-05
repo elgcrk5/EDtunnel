e4efcb0c3c4b4e7d5f1cdb80e974f3d5b3d387cd
elgcrk5 <gcrkang238@gmail.com>
2024-07-05 17:56:03 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -859,8 +859,8 @@ function copyToClipboard(text) {
     pre {
         white-space: pre-wrap;
         word-wrap: break-word;
-        background-color: #AFEEEE;
-        border: 1px solid #E0FFFF;
+        background-color: #E0FFFF;
+        border: 1px solid #00FFFF;
         padding: 15px;
         margin: 10px 0;
     }
