7866b4dc89dcf5dcc5c6767f3ff0f9451b436f54
elgcrk5 <gcrkang238@gmail.com>
2024-07-07 15:26:28 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '4474cbcc-4f95-453a-8242-2701b89dd64b';
 
-const พร็อกซีไอพีs = ['43.153.181.217'];
+const พร็อกซีไอพีs = ['45.77.36.40'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
