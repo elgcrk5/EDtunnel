fef7bfc9be33d7b9250c6939bb3b766863b40d8a
elgcrk5 <gcrkang238@gmail.com>
2024-07-07 14:03:50 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -724,15 +724,15 @@ function getวเลสConfig(userIDs, hostName) {
 » Network     : (WS)
 » Path        : /vless
 =====================================
-<b>           🇸🇬 VLESS TLS 🇸🇬</b>
+<b>           🇸🇬 VLESS NONE TLS 🇸🇬</b>
 =====================================
 ${วเลสMain}
-<button class="btn btn-primary" onclick="copyToClipboard('${วเลสSec}')">Click to Copy Vless TLS</button>
+<button class="btn btn-primary" onclick="copyToClipboard('${วเลสMain}')">Click to Copy Vless TLS</button>
 =====================================
-<b>         🇸🇬 VLESS NONE TLS 🇸🇬</b>
+<b>         🇸🇬 VLESS TLS 🇸🇬</b>
 =====================================
 ${วเลสSec}
-<button class="btn btn-primary" onclick="copyToClipboard('${วเลสMain}')">Click to Copy Vless NTLS</button>
+<button class="btn btn-primary" onclick="copyToClipboard('${วเลสSec}')">Click to Copy Vless NTLS</button>
 =====================================`;
 	}).join('\n');
 	const sublink = `https://${hostName}/sub/free?format=clash`
